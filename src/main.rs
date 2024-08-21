e1f648a32d46eae97d85fe9ee3f70d0b716e9f1c
Waradu <waradu@outlook.com>
2024-08-21 11:03:41 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![windows_subsystem = "windows"]
+
+use bevy::{
+    prelude::*,
+    window::{Cursor, PrimaryWindow},
+};
+use bevy_embedded_assets::{EmbeddedAssetPlugin, PluginMode};
+use rand::Rng;
+
+#[derive(Component)]
+struct MainCamera;
+
+fn main() {
+    let window = Window {
+        title: "Template".into(),
+        name: Some("template.app".into()),
+        resolution: (800., 500.).into(),
+        ..default()
+    };
+
+    App::new()
+        .add_plugins((
+            EmbeddedAssetPlugin {
+                mode: PluginMode::ReplaceDefault,
+                ..default()
+            },
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(window),
+                    ..default()
+                }),
+        ))
+        .add_systems(Startup, startup)
+        .run();
+}
+
+fn startup(mut commands: Commands) {
+    commands.spawn((Camera2dBundle::default(), MainCamera));
+}
